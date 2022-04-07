b2ddec853bd29d059f9631e70ba665ce34d7c879
Udoka Christopher <udokachris25@gmail.com>
2022-04-07 07:30:55 -0700

    My task number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 /**
- * main - entry point
- * Return: 0 after printing the function
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  * */
-int main(void)
+int main(void) 
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return(0);
 }
