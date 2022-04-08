f7dc080b6cff82dbdad5b500ed8e17260d2fa3e9
Udoka Christopher <udokachris25@gmail.com>
2022-04-08 07:53:51 -0700

    positive energy only

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,36 +1,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/* more headers goes there */
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success/correct)
+ * Return: Always 0 (success)
  */
 
+/* betty style doc for function main goes there */
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-if (n==0)
-{
-printf("%i is zero\n", n);
-}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n",);
+	}
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
 
-else if (n<0)
-{
-printf("%i is negative\n", n);
-}
-
-else
-{
-printf("%i is positive\n", n);
-}
-
-return (0)
+	return (0);
 
 }
-
