3971fb641e7e6ef745b5be01d5cc03d9a87b4cee
Udoka Christopher <udokachris25@gmail.com>
2022-04-07 17:48:49 -0700

    my task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,9 +7,6 @@
  */
 int main(void)
 {
-	char str{} = "with proper grammar, but the outcome is a piece of art,";
-
-	printf("%s\n", str);
-
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
