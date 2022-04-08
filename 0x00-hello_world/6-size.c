3e013a32a47abb5a1adf9da4e941a7d0beddd6e9
Udoka Christopher <udokachris25@gmail.com>
2022-04-07 22:15:07 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a; 
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("size of a char: %zu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of an int: %zu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a long int: %zu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long long int: %zu byte(s)\n", (unsignedlong)sizeof(c));
+printf("size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
+
