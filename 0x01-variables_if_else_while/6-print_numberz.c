4177611346f665e2c11f389ee430fdb1048e4196
Udoka Christopher <udokachris25@gmail.com>
2022-04-09 07:15:17 -0700

    print number 5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * program that prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int g = 0;
+
+	if (g < 10)
+
+	{
+		putchar(g + '0');
+	}
+	putchar ('\n');
+	return (0);
+}
